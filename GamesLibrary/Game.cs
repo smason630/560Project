49ffe84df54e2ae08f2c986eb6c6228b283179a1
zbnelson <zbnelson@ksu.edu>
2020-04-27 21:00:22 -0500

    added game object, add view roughdraft

--- GamesLibrary/Game.cs
+++ GamesLibrary/Game.cs
@@ -8,5 +8,21 @@ namespace GamesLibrary
 {
     class Game
     {
+        private string Name { get; set; }
+        private string Genre { get; set; }
+        private string Console { get; set; }
+        private string Publisher { get; set; }
+        private string Developer { get; set; }
+        private int ReleaseYear { get; set; }
+
+        public Game(string n, string g, string c, string p, string d, int y)
+        {
+            Name = n;
+            Genre = g;
+            Console = c;
+            Publisher = p;
+            Developer = d;
+            ReleaseYear = y;
+        }
     }
 }
