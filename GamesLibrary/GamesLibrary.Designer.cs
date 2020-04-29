9e0f9835cafa7902821645eb20d45c9382052ea1
CommandoFernando <46908071+CommandoFernando@users.noreply.github.com>
2020-04-29 16:17:52 -0500

    added global variables for a sqlconnection, sqladapter, dataset, and bindingsource

--- GamesLibrary/GamesLibrary.Designer.cs
+++ GamesLibrary/GamesLibrary.Designer.cs
@@ -42,43 +42,48 @@
             // 
             // UX_PublisherButton
             // 
-            this.UX_PublisherButton.Location = new System.Drawing.Point(259, 26);
+            this.UX_PublisherButton.Location = new System.Drawing.Point(345, 32);
+            this.UX_PublisherButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_PublisherButton.Name = "UX_PublisherButton";
-            this.UX_PublisherButton.Size = new System.Drawing.Size(75, 20);
+            this.UX_PublisherButton.Size = new System.Drawing.Size(100, 25);
             this.UX_PublisherButton.TabIndex = 2;
             this.UX_PublisherButton.Text = "Publisher";
             this.UX_PublisherButton.UseVisualStyleBackColor = true;
             // 
             // UX_DeveloperButton
             // 
-            this.UX_DeveloperButton.Location = new System.Drawing.Point(357, 26);
+            this.UX_DeveloperButton.Location = new System.Drawing.Point(476, 32);
+            this.UX_DeveloperButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_DeveloperButton.Name = "UX_DeveloperButton";
-            this.UX_DeveloperButton.Size = new System.Drawing.Size(75, 20);
+            this.UX_DeveloperButton.Size = new System.Drawing.Size(100, 25);
             this.UX_DeveloperButton.TabIndex = 3;
             this.UX_DeveloperButton.Text = "Developer";
             this.UX_DeveloperButton.UseVisualStyleBackColor = true;
             // 
             // UX_ResetButton
             // 
-            this.UX_ResetButton.Location = new System.Drawing.Point(779, 23);
+            this.UX_ResetButton.Location = new System.Drawing.Point(1039, 28);
+            this.UX_ResetButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_ResetButton.Name = "UX_ResetButton";
-            this.UX_ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.UX_ResetButton.Size = new System.Drawing.Size(100, 28);
             this.UX_ResetButton.TabIndex = 4;
             this.UX_ResetButton.Text = "Reset";
             this.UX_ResetButton.UseVisualStyleBackColor = true;
             // 
             // UX_SearchBar
             // 
-            this.UX_SearchBar.Location = new System.Drawing.Point(512, 26);
+            this.UX_SearchBar.Location = new System.Drawing.Point(683, 32);
+            this.UX_SearchBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_SearchBar.Name = "UX_SearchBar";
-            this.UX_SearchBar.Size = new System.Drawing.Size(100, 20);
+            this.UX_SearchBar.Size = new System.Drawing.Size(132, 22);
             this.UX_SearchBar.TabIndex = 5;
             // 
             // UX_SearchButton
             // 
-            this.UX_SearchButton.Location = new System.Drawing.Point(618, 26);
+            this.UX_SearchButton.Location = new System.Drawing.Point(824, 32);
+            this.UX_SearchButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_SearchButton.Name = "UX_SearchButton";
-            this.UX_SearchButton.Size = new System.Drawing.Size(75, 20);
+            this.UX_SearchButton.Size = new System.Drawing.Size(100, 25);
             this.UX_SearchButton.TabIndex = 6;
             this.UX_SearchButton.Text = "Search";
             this.UX_SearchButton.UseVisualStyleBackColor = true;
@@ -86,43 +91,48 @@
             // UX_Table
             // 
             this.UX_Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.UX_Table.Location = new System.Drawing.Point(12, 76);
+            this.UX_Table.Location = new System.Drawing.Point(16, 94);
+            this.UX_Table.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_Table.Name = "UX_Table";
-            this.UX_Table.Size = new System.Drawing.Size(842, 368);
+            this.UX_Table.RowHeadersWidth = 51;
+            this.UX_Table.Size = new System.Drawing.Size(1123, 453);
             this.UX_Table.TabIndex = 7;
             // 
             // UX_GenreDropDown
             // 
             this.UX_GenreDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.UX_GenreDropDown.FormattingEnabled = true;
-            this.UX_GenreDropDown.Location = new System.Drawing.Point(12, 25);
+            this.UX_GenreDropDown.Location = new System.Drawing.Point(16, 31);
+            this.UX_GenreDropDown.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_GenreDropDown.Name = "UX_GenreDropDown";
-            this.UX_GenreDropDown.Size = new System.Drawing.Size(97, 21);
+            this.UX_GenreDropDown.Size = new System.Drawing.Size(128, 24);
             this.UX_GenreDropDown.TabIndex = 9;
             // 
             // UX_ConsoleDropDown
             // 
             this.UX_ConsoleDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.UX_ConsoleDropDown.FormattingEnabled = true;
-            this.UX_ConsoleDropDown.Location = new System.Drawing.Point(136, 25);
+            this.UX_ConsoleDropDown.Location = new System.Drawing.Point(181, 31);
+            this.UX_ConsoleDropDown.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_ConsoleDropDown.Name = "UX_ConsoleDropDown";
-            this.UX_ConsoleDropDown.Size = new System.Drawing.Size(97, 21);
+            this.UX_ConsoleDropDown.Size = new System.Drawing.Size(128, 24);
             this.UX_ConsoleDropDown.TabIndex = 10;
             // 
             // UX_AddButton
             // 
-            this.UX_AddButton.Location = new System.Drawing.Point(779, 457);
+            this.UX_AddButton.Location = new System.Drawing.Point(1039, 562);
+            this.UX_AddButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UX_AddButton.Name = "UX_AddButton";
-            this.UX_AddButton.Size = new System.Drawing.Size(75, 23);
+            this.UX_AddButton.Size = new System.Drawing.Size(100, 28);
             this.UX_AddButton.TabIndex = 11;
             this.UX_AddButton.Text = "Add";
             this.UX_AddButton.UseVisualStyleBackColor = true;
             // 
             // GamesLibrary
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(866, 492);
+            this.ClientSize = new System.Drawing.Size(1155, 606);
             this.Controls.Add(this.UX_AddButton);
             this.Controls.Add(this.UX_ConsoleDropDown);
             this.Controls.Add(this.UX_GenreDropDown);
@@ -132,8 +142,9 @@
             this.Controls.Add(this.UX_ResetButton);
             this.Controls.Add(this.UX_DeveloperButton);
             this.Controls.Add(this.UX_PublisherButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GamesLibrary";
-            this.Text = "Form1";
+            this.Text = "Games";
             ((System.ComponentModel.ISupportInitialize)(this.UX_Table)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- GamesLibrary/GamesLibrary.cs
+++ GamesLibrary/GamesLibrary.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,11 +17,32 @@ namespace GamesLibrary
     public partial class GamesLibrary : Form
     {
         /// <summary>
+        /// global variables used for testing
+        /// </summary>
+        static string ferncon = "Data Source=OMNIUS\\SQLEXPRESS;Initial Catalog=TeamProject;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(ferncon);
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        string query = "select * from GamesLibrary.Games";
+        public DataSet dataset = new DataSet();
+        public BindingSource bs = new BindingSource();
+        /// <summary>
         /// Default implementation
         /// </summary>
         public GamesLibrary()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// doesnt do anything, fernando added this
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GamesLibrary_Load(object sender, EventArgs e)
+        {
+            adapter.SelectCommand = new SqlCommand(query, connection);
+            adapter.Fill(dataset);
+            bs.DataSource = dataset;
+            UX_Table.DataSource = bs;
         }
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
